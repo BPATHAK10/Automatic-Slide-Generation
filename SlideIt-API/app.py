82530aee2451aebef88d52442891bc063534bc95
Biraj <birajpathak10@gmail.com>
2022-08-15 19:01:06 +0545

    integrated slides and drive, now need to respond with the pdf file to the frontend

--- SlideIt-API/app.py
+++ SlideIt-API/app.py
@@ -1,10 +1,9 @@
-from flask import Flask,render_template,url_for,request
-# import pandas as pd 
+from flask import Flask, render_template, url_for, request
+# import pandas as pd
 import pickle
-from GoogleSlides import slides
+import slides
 
 from flask import jsonify
-
 
 # load the model from disk
 # filename = 'model.pkl'
@@ -12,65 +11,41 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-	return jsonify({'message':'Welcome to the SlideIt-API'}) 
+	return jsonify({'message': 'Welcome to the SlideIt-API'})
 
-@app.route('/predict',methods=['POST','GET'])
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-#	df= pd.read_csv("spam.csv", encoding="latin-1")
-#	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-#	# Features and Labels
-#	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-#	X = df['message']
-#	y = df['label']
-#	
-#	# Extract Feature With CountVectorizer
-#	cv = CountVectorizer()
-#	X = cv.fit_transform(X) # Fit the Data
-#    
-#    pickle.dump(cv, open('tranform.pkl', 'wb'))
-#    
-#    
-#	from sklearn.model_selection import train_test_split
-#	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-#	#Naive Bayes Classifier
-#	from sklearn.naive_bayes import MultinomialNB
-#
-#	clf = MultinomialNB()
-#	clf.fit(X_train,y_train)
-#	clf.score(X_test,y_test)
-#    filename = 'nlp_model.pkl'
-#    pickle.dump(clf, open(filename, 'wb'))
-    
-	#Alternative Usage of Saved Model
-	# joblib.dump(clf, 'NB_spam_model.pkl')
-	# NB_spam_model = open('NB_spam_model.pkl','rb')
-	# clf = joblib.load(NB_spam_model)
 
-	# if request.method == 'POST':
-	# 	message = request.form['message']
-	# 	data = [message]
-	# 	vect = cv.transform(data).toarray()
-	# 	my_prediction = clf.predict(vect)
-	# return render_template('result.html',prediction = my_prediction)
+	if request.method == 'POST':
+		# extract the prediction from the model
 
-    if request.method == 'POST':
-        ## extract the prediction from the model
+		# presentationContent = request.get_json()
+		# print(presentationContent)
 
-        presentationContent = request.get_json()
-        print(presentationContent)
+		presentationContent = {
+			"title": "Our first slide",
+			"subtitle": "This is our first slide",
+			"slides": [
+				[
+					"this is a bullet point",
+					"this is another bullet point",
+					" this is another bullet point",
+				],
+			],
+		}
+		
 
-        generatedSlides = slides.generateSlidesFromAPI(presentationContent)
-        print(generatedSlides)
+		generatedSlides = slides.generateSlidesFromAPI(presentationContent)
+		print(generatedSlides)
 
-        return jsonify({'message': "you now get the slides pdf"})
-    
+		return jsonify({'message': "you now get the slides pdf"})
 
-
-    if request.method == 'GET':
-        return jsonify({'message':'this is the summary'})
-
+	if request.method == 'GET':
+		return jsonify({'message': 'Please use the POST method'})	
 
 
 if __name__ == '__main__':
